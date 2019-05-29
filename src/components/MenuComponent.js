2f26b2f915b927239c50b75b3386202858d046b6
Gina Marcela Cruz Garzon <gmcruz@unicauca.edu.co>
2019-05-29 06:12:49 -0500

    props theme

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Media, Card,CardBody,CardTitle,CardSubtitle, CardImg, CardText } from 'reactstrap';
+
+class Menu extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedDish: dish
+        }
+    }
+
+    onDishSelected(dish){
+        this.setState({selectedDish:dish});
+        
+    }
+
+    
+  
+    render() {
+        const menu = this.props.dishes.map((dish) => {
+            return (
+                <div key={dish.id} className='col-12 col-sm-6  col-md-4 col-lg-3 mt-4'>
+                   <Card onClick={()=> this.onDishSelected(dish)}>
+                       <CardImg src={dish.image} alt={dish.name} />
+                       <CardBody>
+                           <CardTitle>{dish.name}</CardTitle>
+                           <CardSubtitle>{dish.category}</CardSubtitle>
+                           <CardText>{dish.description}</CardText>
+                       </CardBody>
+                   </Card>
+
+                                   </div>
+            );
+        });
+
+        return (
+            <div className='container'>
+
+                <div className='row'>
+                    {menu}
+                </div>
+            </div>
+
+        )
+    };
+}
+
+export default Menu;
